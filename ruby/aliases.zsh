9315e1ef39e8d4cc32ba96d9ef9ef1910ebc4108
AvnerCohen <israbirding@gmail.com>
2012-09-24 10:12:38 +0200

    alias to annotate

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -4,4 +4,5 @@ alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
-alias migrate='rake db:migrate db:test:clone'
+alias migrate='rake db:migrate db:test:clone' # migrate db and clone testing db
+alias annotate='bundle exec annotate' # re-annotate models
