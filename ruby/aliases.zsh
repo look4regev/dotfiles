cef5ad285b377fb72b8a4d4cd03bbcf66dbce40f
AvnerCohen <israbirding@gmail.com>
2012-09-24 23:50:32 +0200

    reake remigrate alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -6,3 +6,5 @@ alias sd='script/destroy'
 
 alias migrate='rake db:migrate db:test:clone' # migrate db and clone testing db
 alias annotate='bundle exec annotate' # re-annotate models
+#Can be used when db goes all fucked up, active record complains and rake db:migare wont fix it.
+alias remigrate='rake db:drop && rake db:create && rake db:migrate && rake db:schema:dump && rake db:test:prepare' 
