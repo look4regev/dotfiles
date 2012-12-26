7793d5e3d8dbad43c8478cf9329dffe875953a82
Avner Cohen <avner.cohen@fiverr.com>
2012-12-26 12:03:20 +0200

    various changes

--- redis/aliases.zsh
+++ redis/aliases.zsh
@@ -1,2 +1,4 @@
-alias redis-up="redis-server $ZSH/redis/redis.conf > /dev/null &"
+#alias redis-up="redis-server $ZSH/redis/redis.conf > /dev/null &"
+alias redis-up="redis-server > /dev/null &"
+
 alias redis-down='killall redis-server'
