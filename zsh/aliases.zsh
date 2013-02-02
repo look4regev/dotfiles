736638c7be2c21457ebbb294dccdbc6be896a42c
Avner Cohen <avner.cohen@fiverr.com>
2013-02-02 14:50:03 +0200

    local nodemodules and monitorin

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,4 @@ alias set_heroku='~/dev/repos/shorts/set_heroku_env.sh'
 alias ls='ls -lah'
 alias history='history -100'
 alias clean_routes="rake routes | sed -e '1d' -e 's,^[^/]*,,g' | awk '{print $1}' | sort | uniq"
+alias m="ssh admin@monitorin"
