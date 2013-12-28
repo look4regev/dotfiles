5755f2005d15fcdd85033c068f672eaadcadaa6d
AvnerCohen <israbirding@gmail.com>
2013-12-28 18:48:54 +0200

    Added node modules bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:./node_modules/.bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
