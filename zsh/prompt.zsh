d359dfd764a653948960ceb9adeb4c40c9c41921
AvnerCohen <israbirding@gmail.com>
2016-02-14 17:03:01 +0200

    [FIX] Adding python virtual env to PS1

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -43,36 +43,27 @@ need_push () {
 rb_prompt(){
   if $(which rbenv &> /dev/null)
   then
-	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
+	  echo "rb: %{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
 	else
 	  echo ""
   fi
 }
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
+virtual_env(){
+  if [ -z ${VIRTUAL_ENV+x} ]
   then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
+  echo ""
   else
-    echo ""
+  echo " (py: %{$fg_bold[yellow]%}$(basename "$VIRTUAL_ENV"))%{$reset_color%}"
   fi
 }
+
 
 directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n[$(rb_prompt) $(virtual_env)] in $(directory_name) $(git_dirty)$(need_push)\n› '
 ## Using a newline in here requirs a change to sublime text, if you want to run ruby test.
 ## Should should be to change line 45 of exec.py, found in 
 # ~/Library/Application Support/Sublime Text 2/Packages/Default, to:
@@ -81,10 +72,3 @@ export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n�
 # $ python
 # >>> import py_compile
 # >>> py_compile.compile('exec.y')
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
-
-precmd() {
-  set_prompt
-}
