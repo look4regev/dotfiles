07acd5e88feb5a3f0b91960d2d70523d251a06c2
AvnerCohen <israbirding@gmail.com>
2016-02-28 11:32:03 +0200

    [FEAT] Save python REPL history

--- zsh/pythonenv.zsh
+++ zsh/pythonenv.zsh
@@ -1,4 +1,5 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/HSCode
 export PIP_REQUIRE_VIRTUALENV=true
+export PYTHONSTARTUP=~/.dotfiles/python/.pystartup
 . /usr/local/bin/virtualenvwrapper.sh
