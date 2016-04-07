28a311763417975bd4ea36c2845a2c57082b1e42
Avner Cohen <israbirding@gmail.com>
2016-04-07 13:25:29 +0300

    [FIX] Python no pyc - colloquy added to cask installes

--- zsh/pythonenv.zsh
+++ zsh/pythonenv.zsh
@@ -2,4 +2,5 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/HSCode
 export PIP_REQUIRE_VIRTUALENV=true
 export PYTHONSTARTUP=~/.dotfiles/python/.pystartup
+export PYATHONDONTWRITEBYTECODE=1
 . /usr/local/bin/virtualenvwrapper.sh
