f3e4356c4322966da391b83b2693482dedea2717
Avner Cohen <israbirding@gmail.com>
2016-04-25 12:54:16 +0300

    [FIX[ Minor change to mou pointer

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 alias reload!='. ~/.zshrc'
-alias mou='/Applications/Mou.app/Contents/MacOS/Mou'
+alias mou='~/Applications/Mou.app'
 alias bi='bundle install'
 alias ginit='git init && git add . && git commit -am "Initial commit"'
 alias set_heroku='~/dev/repos/shorts/set_heroku_env.sh'
--- zsh/pythonenv.zsh
+++ zsh/pythonenv.zsh
@@ -3,4 +3,7 @@ export PROJECT_HOME=$HOME/HSCode
 export PIP_REQUIRE_VIRTUALENV=true
 export PYTHONSTARTUP=~/.dotfiles/python/.pystartup
 export PYATHONDONTWRITEBYTECODE=1
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
 . /usr/local/bin/virtualenvwrapper.sh
