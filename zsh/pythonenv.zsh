1476dc735ec401cf74967b36460d1fb8d15ab4e9
Avner Cohen <israbirding@gmail.com>
2016-05-30 17:54:39 +0300

    [FIX] corrected pyc removal spelling

--- zsh/pythonenv.zsh
+++ zsh/pythonenv.zsh
@@ -2,7 +2,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/HSCode
 export PIP_REQUIRE_VIRTUALENV=true
 export PYTHONSTARTUP=~/.dotfiles/python/.pystartup
-export PYATHONDONTWRITEBYTECODE=1
+export PYTHONDONTWRITEBYTECODE=1
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
