3884703afe060e5a54a826c76e03225ef41218c2
Avner Cohen <israbirding@gmail.com>
2017-08-16 11:20:18 +0300

    Remove vim gitignore, cayses some

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
 export PATH="./bin:./node_modules/.bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
