b066102246ea335e94df39b06c2dd9693b825a50
Avner Cohen <israbirding@gmail.com>
2017-11-14 16:29:00 +0200

    PATH and comp cleanup

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH="./bin:./node_modules/.bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:./node_modules/.bin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$(brew --prefix homebrew/php/php56)/bin:$PATH"
