17168c21117946f0aea15b96e30e1734f1694965
Regev Golan <look4regev@gmail.com>
2020-10-18 16:47:46 +0300

    [osx] Disable annoying sound effects (screenshot and file delete for example)

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -39,3 +39,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Disable annoying sound effects (screenshot and file delete for example)
+defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
